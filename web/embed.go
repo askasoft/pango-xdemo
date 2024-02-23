ef88579c0c268236e3b38ceffe5dbb6a8bbfd27f
Frank Wang <yf.frank.wang@outlook.com>
2024-02-24 08:51:19 +0900

    upgrade to fontawesome6

--- web/embed.go
+++ web/embed.go
@@ -6,7 +6,7 @@ import (
 	"github.com/askasoft/pango-assets/html/bootstrap5"
 	"github.com/askasoft/pango-assets/html/bootswatch5"
 	"github.com/askasoft/pango-assets/html/corejs"
-	"github.com/askasoft/pango-assets/html/fontawesome4"
+	"github.com/askasoft/pango-assets/html/fontawesome6"
 	"github.com/askasoft/pango-assets/html/jquery"
 	"github.com/askasoft/pango-assets/html/plugins"
 )
@@ -20,7 +20,7 @@ var Favicon []byte
 var Statics = map[string]embed.FS{
 	"bootstrap5":   bootstrap5.FS,
 	"bootswatch5":  bootswatch5.FS,
-	"fontawesome4": fontawesome4.FS,
+	"fontawesome6": fontawesome6.FS,
 	"corejs":       corejs.FS,
 	"jquery":       jquery.FS,
 	"plugins":      plugins.FS,
