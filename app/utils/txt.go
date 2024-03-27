0c180e1c2c5124c82a1c5008959605d35cdd46a2
Frank Wang <yf.frank.wang@outlook.com>
2024-03-28 00:54:01 +0900

    utils: add GetAllReverseMap(), remove FindKeyByValue()

--- app/utils/txt.go
+++ app/utils/txt.go
@@ -1,38 +1,29 @@
 package utils
 
 import (
+	"encoding/json"
+
 	"github.com/askasoft/pango-xdemo/app"
 	"github.com/askasoft/pango/cog"
+	"github.com/askasoft/pango/mag"
 	"github.com/askasoft/pango/str"
 	"github.com/askasoft/pango/tbs"
 )
-
-func FindKeyByValue(name string, val string) (string, bool) {
-	for _, lang := range app.Locales {
-		lm := GetLinkedHashMap(lang, name)
-		key, ok := FindKeyByValueInMap(lm, val)
-		if ok {
-			return key, ok
-		}
-	}
-	return "", false
-}
-
-func FindKeyByValueInMap(lm *cog.LinkedHashMap[string, string], v string) (string, bool) {
-	for it := lm.Iterator(); it.Next(); {
-		if it.Value() == v {
-			return it.Key(), true
-		}
-	}
-	return "", false
-}
 
 func GetUserStatusMap(locale string) *cog.LinkedHashMap[string, string] {
 	return GetLinkedHashMap(locale, "user.map.status")
 }
 
+func GetUserStatusReverseMap() map[string]string {
+	return GetAllReverseMap("user.map.status")
+}
+
 func GetUserRoleMap(locale string) *cog.LinkedHashMap[string, string] {
 	return GetLinkedHashMap(locale, "user.map.role")
+}
+
+func GetUserRoleReverseMap() map[string]string {
+	return GetAllReverseMap("user.map.role")
 }
 
 func GetSuperRoleMap(locale string) *cog.LinkedHashMap[string, string] {
@@ -41,6 +32,29 @@ func GetSuperRoleMap(locale string) *cog.LinkedHashMap[string, string] {
 
 func GetLinkedHashMap(locale, name string) *cog.LinkedHashMap[string, string] {
 	m := &cog.LinkedHashMap[string, string]{}
-	_ = m.UnmarshalJSON(str.UnsafeBytes(tbs.GetText(locale, name)))
+	err := m.UnmarshalJSON(str.UnsafeBytes(tbs.GetText(locale, name)))
+	if err != nil {
+		panic(err)
+	}
 	return m
+}
+
+func GetReverseMap(locale, name string) map[string]string {
+	m := make(map[string]string)
+	err := json.Unmarshal(str.UnsafeBytes(tbs.GetText(locale, name)), &m)
+	if err != nil {
+		panic(err)
+	}
+	return m
+}
+
+func GetAllReverseMap(name string) map[string]string {
+	am := make(map[string]string)
+
+	for _, lang := range app.Locales {
+		rm := GetReverseMap(lang, name)
+		mag.Copy(am, rm)
+	}
+
+	return am
 }
