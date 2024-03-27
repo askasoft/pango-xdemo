b73561b59a12acc32c932a612db1874d4b54c2c2
Frank Wang <yf.frank.wang@outlook.com>
2024-03-27 22:02:06 +0900

    change Expiry to Expires

--- app/tasks/tempfile_cleaner.go
+++ app/tasks/tempfile_cleaner.go
@@ -12,7 +12,7 @@ import (
 
 func CleanTemporaryFiles() {
 	prefix := "/" + models.PrefixTmpFile + "/"
-	before := time.Now().Add(-1 * app.INI.GetDuration("app", "tempfileExpiry", time.Hour*8))
+	before := time.Now().Add(-1 * app.INI.GetDuration("app", "tempfileExpires", time.Hour*8))
 
 	_ = tenant.Iterate(func(tt tenant.Tenant) error {
 		gfs := gormfs.FS(app.DB, tt.TableFiles())
