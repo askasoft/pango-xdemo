4eddc6c64c968418aca2cfff93096252b57426b2
Frank Wang <yf.frank.wang@outlook.com>
2024-08-06 13:39:23 +0900

    embed: fix bootswatch5 embed FS

--- web/embed.go
+++ web/embed.go
@@ -4,7 +4,9 @@ import (
 	"embed"
 
 	"github.com/askasoft/pango-assets/html/bootstrap5"
-	"github.com/askasoft/pango-assets/html/bootswatch5"
+	"github.com/askasoft/pango-assets/html/bootswatch5/cosmo"
+	"github.com/askasoft/pango-assets/html/bootswatch5/flatly"
+	"github.com/askasoft/pango-assets/html/bootswatch5/pulse"
 	"github.com/askasoft/pango-assets/html/corejs"
 	"github.com/askasoft/pango-assets/html/fontawesome6"
 	"github.com/askasoft/pango-assets/html/jquery"
@@ -13,12 +15,14 @@ import (
 
 // Static embed static folder
 var Statics = map[string]embed.FS{
-	"bootstrap5":   bootstrap5.FS,
-	"bootswatch5":  bootswatch5.FS,
-	"fontawesome6": fontawesome6.FS,
-	"corejs":       corejs.FS,
-	"jquery":       jquery.FS,
-	"plugins":      plugins.FS,
+	"bootstrap5":         bootstrap5.FS,
+	"bootswatch5/cosmo":  cosmo.FS,
+	"bootswatch5/flatly": flatly.FS,
+	"bootswatch5/pulse":  pulse.FS,
+	"fontawesome6":       fontawesome6.FS,
+	"corejs":             corejs.FS,
+	"jquery":             jquery.FS,
+	"plugins":            plugins.FS,
 }
 
 //go:embed assets favicon.ico
