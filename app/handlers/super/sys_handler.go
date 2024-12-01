d5ee2f1589662b052a6cff5df88231ddb4a7f3fd
Frank Wang <yf.frank.wang@outlook.com>
2024-12-01 21:34:50 +0900

    super/sys: use card flex layout

--- app/handlers/super/sys_handler.go
+++ app/handlers/super/sys_handler.go
@@ -1,13 +1,12 @@
 package super
 
 import (
-	"fmt"
 	"net/http"
 	"runtime"
 
 	"github.com/askasoft/pango-xdemo/app/handlers"
+	"github.com/askasoft/pango/cog/linkedhashmap"
 	"github.com/askasoft/pango/num"
-	"github.com/askasoft/pango/str"
 	"github.com/askasoft/pango/xin"
 )
 
@@ -17,30 +16,30 @@ func SysStats(c *xin.Context) {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
 
-	sb := &str.Builder{}
+	lmap := linkedhashmap.NewLinkedHashMap[string, string]()
 
-	fmt.Fprintf(sb, "         CPU: %d\n", runtime.NumCPU())
-	fmt.Fprintf(sb, "   Goroutine: %d\n", runtime.NumGoroutine())
-	fmt.Fprintf(sb, "     Mallocs: %s\n", num.HumanSize(float64(m.Mallocs)))
-	fmt.Fprintf(sb, "       Frees: %s\n", num.Comma(m.Frees))
-	fmt.Fprintf(sb, "       Alloc: %s\n", num.HumanSize(float64(m.Alloc)))
-	fmt.Fprintf(sb, "   HeapAlloc: %s\n", num.HumanSize(float64(m.HeapAlloc)))
-	fmt.Fprintf(sb, "  TotalAlloc: %s\n", num.HumanSize(float64(m.TotalAlloc)))
-	fmt.Fprintf(sb, "         Sys: %s\n", num.HumanSize(float64(m.Sys)))
-	fmt.Fprintf(sb, "     Lookups: %s\n", num.Comma(m.Lookups))
-	fmt.Fprintf(sb, "   HeapAlloc: %s\n", num.HumanSize(float64(m.HeapAlloc)))
-	fmt.Fprintf(sb, "    HeapIdle: %s\n", num.HumanSize(float64(m.HeapIdle)))
-	fmt.Fprintf(sb, "   HeapInuse: %s\n", num.HumanSize(float64(m.HeapInuse)))
-	fmt.Fprintf(sb, "HeapReleased: %s\n", num.HumanSize(float64(m.HeapReleased)))
-	fmt.Fprintf(sb, " HeapObjects: %s\n", num.Comma(m.HeapObjects))
-	fmt.Fprintf(sb, "  StackInuse: %s\n", num.HumanSize(float64(m.StackInuse)))
-	fmt.Fprintf(sb, "    StackSys: %s\n", num.HumanSize(float64(m.StackSys)))
-	fmt.Fprintf(sb, "  MSpanInuse: %s\n", num.HumanSize(float64(m.MSpanInuse)))
-	fmt.Fprintf(sb, "    MSpanSys: %s\n", num.HumanSize(float64(m.MSpanSys)))
-	fmt.Fprintf(sb, " MCacheInuse: %s\n", num.HumanSize(float64(m.MCacheInuse)))
-	fmt.Fprintf(sb, "   MCacheSys: %s\n", num.HumanSize(float64(m.MCacheSys)))
+	lmap.Set("CPU", num.Comma(runtime.NumCPU()))
+	lmap.Set("Goroutine", num.Comma(runtime.NumGoroutine()))
+	lmap.Set("Mallocs", num.HumanSize(float64(m.Mallocs)))
+	lmap.Set("Frees", num.Comma(m.Frees))
+	lmap.Set("Alloc", num.HumanSize(float64(m.Alloc)))
+	lmap.Set("HeapAlloc", num.HumanSize(float64(m.HeapAlloc)))
+	lmap.Set("TotalAlloc", num.HumanSize(float64(m.TotalAlloc)))
+	lmap.Set("Sys", num.HumanSize(float64(m.Sys)))
+	lmap.Set("Lookups", num.Comma(m.Lookups))
+	lmap.Set("HeapAlloc", num.HumanSize(float64(m.HeapAlloc)))
+	lmap.Set("HeapIdle", num.HumanSize(float64(m.HeapIdle)))
+	lmap.Set("HeapInuse", num.HumanSize(float64(m.HeapInuse)))
+	lmap.Set("HeapReleased", num.HumanSize(float64(m.HeapReleased)))
+	lmap.Set("HeapObjects", num.Comma(m.HeapObjects))
+	lmap.Set("StackInuse", num.HumanSize(float64(m.StackInuse)))
+	lmap.Set("StackSys", num.HumanSize(float64(m.StackSys)))
+	lmap.Set("MSpanInuse", num.HumanSize(float64(m.MSpanInuse)))
+	lmap.Set("MSpanSys", num.HumanSize(float64(m.MSpanSys)))
+	lmap.Set("MCacheInuse", num.HumanSize(float64(m.MCacheInuse)))
+	lmap.Set("MCacheSys", num.HumanSize(float64(m.MCacheSys)))
 
-	h["Stats"] = sb.String()
+	h["Stats"] = lmap
 
 	c.HTML(http.StatusOK, "super/sys", h)
 }
