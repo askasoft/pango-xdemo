bf2ec89b6c1a62152f69eee77d06e920edfc93d7
Frank Wang <yf.frank.wang@outlook.com>
2025-04-14 18:07:35 +0900

    move update/delete handler from detail_handler to update_handler

---
+++ app/handlers/admin/users/user_update_handler.go
@@ -0,0 +1,341 @@
+package users
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/askasoft/pango-xdemo/app"
+	"github.com/askasoft/pango-xdemo/app/handlers"
+	"github.com/askasoft/pango-xdemo/app/models"
+	"github.com/askasoft/pango-xdemo/app/tenant"
+	"github.com/askasoft/pango-xdemo/app/utils/argutil"
+	"github.com/askasoft/pango-xdemo/app/utils/pgutil"
+	"github.com/askasoft/pango-xdemo/app/utils/pwdutil"
+	"github.com/askasoft/pango-xdemo/app/utils/tbsutil"
+	"github.com/askasoft/pango-xdemo/app/utils/vadutil"
+	"github.com/askasoft/pango/ran"
+	"github.com/askasoft/pango/sqx/sqlx"
+	"github.com/askasoft/pango/tbs"
+	"github.com/askasoft/pango/xin"
+)
+
+func userValidateRole(c *xin.Context, role string) {
+	if role != "" {
+		au := tenant.AuthUser(c)
+		urm := tbsutil.GetUserRoleMap(c.Locale, au.Role)
+		if !urm.Contains(role) {
+			c.AddError(vadutil.ErrInvalidField(c, "user.", "role"))
+		}
+	}
+}
+
+func userValidateStatus(c *xin.Context, status string) {
+	if status != "" {
+		sm := tbsutil.GetUserStatusMap(c.Locale)
+		if !sm.Contains(status) {
+			c.AddError(vadutil.ErrInvalidField(c, "user.", "status"))
+		}
+	}
+}
+
+func userValidatePassword(c *xin.Context, password string) {
+	if password != "" {
+		tt := tenant.FromCtx(c)
+
+		if vs := tt.ValidatePassword(c.Locale, password); len(vs) > 0 {
+			for _, v := range vs {
+				c.AddError(&vadutil.ParamError{
+					Param:   "password",
+					Message: v,
+				})
+			}
+		}
+	}
+}
+
+func userBind(c *xin.Context) *models.User {
+	user := &models.User{}
+	if err := c.Bind(user); err != nil {
+		vadutil.AddBindErrors(c, err, "user.")
+	}
+
+	userValidateRole(c, user.Role)
+	userValidateStatus(c, user.Status)
+	userValidatePassword(c, user.Password)
+	return user
+}
+
+func UserCreate(c *xin.Context) {
+	user := userBind(c)
+	if len(c.Errors) > 0 {
+		c.JSON(http.StatusBadRequest, handlers.E(c))
+		return
+	}
+
+	tt := tenant.FromCtx(c)
+
+	user.ID = 0
+	if user.Password == "" {
+		user.Password = pwdutil.RandomPassword()
+	}
+	user.SetPassword(user.Password)
+	user.Secret = ran.RandInt63()
+	user.CreatedAt = time.Now()
+	user.UpdatedAt = user.CreatedAt
+
+	db := app.SDB
+	sqb := db.Builder()
+	sqb.Insert(tt.TableUsers())
+	sqb.StructNames(user, "id")
+	if !db.SupportLastInsertID() {
+		sqb.Returns("id")
+	}
+	sql := sqb.SQL()
+
+	uid, err := db.NamedCreate(sql, user)
+	if err != nil {
+		if pgutil.IsUniqueViolationError(err) {
+			err = &vadutil.ParamError{
+				Param:   "email",
+				Message: tbs.Format(c.Locale, "user.error.duplicated", tbs.GetText(c.Locale, "user.email", "email"), user.Email),
+			}
+		}
+		c.AddError(err)
+		c.JSON(http.StatusInternalServerError, handlers.E(c))
+		return
+	}
+
+	user.ID = uid
+	user.Password = ""
+	user.Secret = 0
+	c.JSON(http.StatusOK, xin.H{
+		"success": tbs.GetText(c.Locale, "success.created"),
+		"user":    user,
+	})
+}
+
+func UserUpdate(c *xin.Context) {
+	user := userBind(c)
+	if user.ID == 0 {
+		c.AddError(vadutil.ErrInvalidID(c))
+	}
+	if len(c.Errors) > 0 {
+		c.JSON(http.StatusBadRequest, handlers.E(c))
+		return
+	}
+
+	tt := tenant.FromCtx(c)
+	au := tenant.AuthUser(c)
+
+	sqb := app.SDB.Builder()
+
+	if user.Password == "" {
+		sqb.Select("password").From(tt.TableUsers())
+		sqb.Where("id = ?", user.ID)
+		sql, args := sqb.Build()
+
+		eu := &models.User{}
+		err := app.SDB.Get(eu, sql, args...)
+		if err != nil {
+			c.AddError(err)
+			c.JSON(http.StatusInternalServerError, handlers.E(c))
+			return
+		}
+
+		// NOTE: we need re-encrypt password, because password is encrypted by email
+		user.SetPassword(eu.GetPassword())
+	} else {
+		user.SetPassword(user.Password)
+	}
+
+	user.UpdatedAt = time.Now()
+
+	sqb.Reset()
+	sqb.Update(tt.TableUsers())
+	sqb.Setc("name", user.Name)
+	sqb.Setc("email", user.Email)
+	sqb.Setc("password", user.Password)
+	sqb.Setc("role", user.Role)
+	sqb.Setc("status", user.Status)
+	sqb.Setc("cidr", user.CIDR)
+	sqb.Setc("updated_at", user.UpdatedAt)
+	sqb.Where("id = ?", user.ID)
+	sqb.Where("role >= ?", au.Role)
+	sql, args := sqb.Build()
+
+	r, err := app.SDB.Exec(sql, args...)
+	if err != nil {
+		c.AddError(err)
+		c.JSON(http.StatusInternalServerError, handlers.E(c))
+		return
+	}
+
+	user.Password = ""
+
+	cnt, _ := r.RowsAffected()
+
+	c.JSON(http.StatusOK, xin.H{
+		"success": tbs.Format(c.Locale, "user.success.updates", cnt),
+		"user":    user,
+	})
+}
+
+type UserUpdatesArg struct {
+	ID     string  `json:"id,omitempty" form:"id,strip"`
+	Role   string  `json:"role,omitempty" form:"role,strip"`
+	Status string  `json:"status,omitempty" form:"status,strip"`
+	CIDR   *string `json:"cidr,omitempty" form:"cidr,strip" validate:"omitempty,cidrs"`
+}
+
+func (uua *UserUpdatesArg) IsEmpty() bool {
+	return uua.Role == "" && uua.Status == "" && uua.CIDR == nil
+}
+
+func UserUpdates(c *xin.Context) {
+	uua := &UserUpdatesArg{}
+	if err := c.Bind(uua); err != nil {
+		vadutil.AddBindErrors(c, err, "user.")
+	}
+	userValidateRole(c, uua.Role)
+	userValidateStatus(c, uua.Status)
+
+	if uua.IsEmpty() {
+		c.AddError(tbs.Error(c.Locale, "error.request.invalid"))
+	}
+	if len(c.Errors) > 0 {
+		c.JSON(http.StatusBadRequest, handlers.E(c))
+		return
+	}
+
+	ids, ida := argutil.SplitIDs(uua.ID)
+	if len(ids) == 0 && !ida {
+		c.AddError(vadutil.ErrInvalidID(c))
+		c.JSON(http.StatusBadRequest, handlers.E(c))
+		return
+	}
+
+	au := tenant.AuthUser(c)
+	tt := tenant.FromCtx(c)
+
+	sqb := app.SDB.Builder()
+	sqb.Update(tt.TableUsers())
+
+	if uua.Role != "" {
+		sqb.Setc("role", uua.Role)
+	}
+	if uua.Status != "" {
+		sqb.Setc("status", uua.Status)
+	}
+	if uua.CIDR != nil {
+		sqb.Setc("cidr", *uua.CIDR)
+	}
+	sqb.Setc("updated_at", time.Now())
+
+	sqb.Where("id <> ?", au.ID)
+	sqb.Where("role >= ?", au.Role)
+	if len(ids) > 0 {
+		sqb.In("id", ids)
+	}
+
+	sql, args := sqb.Build()
+
+	r, err := app.SDB.Exec(sql, args...)
+	if err != nil {
+		c.AddError(err)
+		c.JSON(http.StatusInternalServerError, handlers.E(c))
+		return
+	}
+
+	cnt, _ := r.RowsAffected()
+
+	c.JSON(http.StatusOK, xin.H{
+		"success": tbs.Format(c.Locale, "user.success.updates", cnt),
+		"updates": uua,
+	})
+}
+
+func UserDeletes(c *xin.Context) {
+	ids, ida := argutil.SplitIDs(c.PostForm("id"))
+	if len(ids) == 0 && !ida {
+		c.AddError(vadutil.ErrInvalidID(c))
+		c.JSON(http.StatusBadRequest, handlers.E(c))
+		return
+	}
+
+	au := tenant.AuthUser(c)
+	tt := tenant.FromCtx(c)
+
+	var cnt int64
+	err := app.SDB.Transaction(func(tx *sqlx.Tx) error {
+		sqb := tx.Builder()
+		sqb.Delete(tt.TableUsers())
+		sqb.Where("id <> ?", au.ID)
+		sqb.Where("role >= ?", au.Role)
+		if len(ids) > 0 {
+			sqb.In("id", ids)
+		}
+		sql, args := sqb.Build()
+
+		r, err := tx.Exec(sql, args...)
+		if err != nil {
+			return err
+		}
+
+		cnt, _ = r.RowsAffected()
+
+		return tt.ResetSequence(tx, "users", models.UserStartID)
+	})
+	if err != nil {
+		c.AddError(err)
+		c.JSON(http.StatusInternalServerError, handlers.E(c))
+		return
+	}
+
+	c.JSON(http.StatusOK, xin.H{
+		"success": tbs.Format(c.Locale, "user.success.deletes", cnt),
+	})
+}
+
+func UserDeleteBatch(c *xin.Context) {
+	uqa, err := bindUserQueryArg(c)
+	if err != nil {
+		vadutil.AddBindErrors(c, err, "user.")
+		c.JSON(http.StatusBadRequest, handlers.E(c))
+		return
+	}
+
+	if !uqa.HasFilter() {
+		c.AddError(tbs.Error(c.Locale, "error.param.nofilter"))
+		c.JSON(http.StatusBadRequest, handlers.E(c))
+		return
+	}
+
+	tt := tenant.FromCtx(c)
+	au := tenant.AuthUser(c)
+
+	var cnt int64
+	err = app.SDB.Transaction(func(tx *sqlx.Tx) error {
+		sqb := tx.Builder()
+		sqb.Delete(tt.TableUsers())
+		sqb.Where("id <> ?", au.ID)
+		uqa.AddWhere(c, sqb)
+		sql, args := sqb.Build()
+
+		r, err := tx.Exec(sql, args...)
+		if err != nil {
+			return err
+		}
+
+		cnt, _ = r.RowsAffected()
+		return tt.ResetSequence(tx, "users", models.UserStartID)
+	})
+	if err != nil {
+		c.AddError(err)
+		c.JSON(http.StatusBadRequest, handlers.E(c))
+		return
+	}
+
+	c.JSON(http.StatusOK, xin.H{
+		"success": tbs.Format(c.Locale, "user.success.deletes", cnt),
+	})
+}
--- app/handlers/demos/pets/pet_detail_handler.go
+++ app/handlers/demos/pets/pet_detail_handler.go
@@ -2,7 +2,6 @@ package pets
 
 import (
 	"errors"
-	"fmt"
 	"net/http"
 	"time"
 
@@ -10,13 +9,9 @@ import (
 	"github.com/askasoft/pango-xdemo/app/handlers"
 	"github.com/askasoft/pango-xdemo/app/models"
 	"github.com/askasoft/pango-xdemo/app/tenant"
-	"github.com/askasoft/pango-xdemo/app/utils/argutil"
-	"github.com/askasoft/pango-xdemo/app/utils/tbsutil"
 	"github.com/askasoft/pango-xdemo/app/utils/vadutil"
 	"github.com/askasoft/pango/num"
-	"github.com/askasoft/pango/sqx/pqx"
 	"github.com/askasoft/pango/sqx/sqlx"
-	"github.com/askasoft/pango/str"
 	"github.com/askasoft/pango/tbs"
 	"github.com/askasoft/pango/xin"
 )
@@ -38,6 +33,14 @@ func PetNew(c *xin.Context) {
 	bindPetMaps(c, h)
 
 	c.HTML(http.StatusOK, "demos/pets/pet_detail_edit", h)
+}
+
+func PetView(c *xin.Context) {
+	petDetail(c, "view")
+}
+
+func PetEdit(c *xin.Context) {
+	petDetail(c, "edit")
 }
 
 func petDetail(c *xin.Context, action string) {
@@ -73,331 +76,4 @@ func petDetail(c *xin.Context, action string) {
 	bindPetMaps(c, h)
 
 	c.HTML(http.StatusOK, "demos/pets/pet_detail_"+action, h)
-}
-
-func PetView(c *xin.Context) {
-	petDetail(c, "view")
-}
-
-func PetEdit(c *xin.Context) {
-	petDetail(c, "edit")
-}
-
-func petBind(c *xin.Context) *PetWithFile {
-	pet := &PetWithFile{}
-	if err := c.Bind(pet); err != nil {
-		vadutil.AddBindErrors(c, err, "pet.")
-	}
-
-	if pet.Gender != "" {
-		pgm := tbsutil.GetPetGenderMap(c.Locale)
-		if !pgm.Contains(pet.Gender) {
-			c.AddError(vadutil.ErrInvalidField(c, "pet.", "gender"))
-		}
-	}
-
-	if pet.Origin != "" {
-		pom := tbsutil.GetPetOriginMap(c.Locale)
-		if !pom.Contains(pet.Origin) {
-			c.AddError(vadutil.ErrInvalidField(c, "pet.", "origin"))
-		}
-	}
-
-	if pet.Temper != "" {
-		ptm := tbsutil.GetPetTemperMap(c.Locale)
-		if !ptm.Contains(pet.Temper) {
-			c.AddError(vadutil.ErrInvalidField(c, "pet.", "temper"))
-		}
-	}
-
-	if len(pet.Habits) > 0 {
-		phm := tbsutil.GetPetHabitsMap(c.Locale)
-		for _, h := range pet.Habits {
-			if !phm.Contains(h) {
-				c.AddError(vadutil.ErrInvalidField(c, "pet.", "habits"))
-				break
-			}
-		}
-	}
-
-	return pet
-}
-
-func PetCreate(c *xin.Context) {
-	pet := petBind(c)
-	if len(c.Errors) > 0 {
-		c.JSON(http.StatusBadRequest, handlers.E(c))
-		return
-	}
-
-	pet.ID = 0
-	pet.CreatedAt = time.Now()
-	pet.UpdatedAt = pet.CreatedAt
-
-	tt := tenant.FromCtx(c)
-
-	err := app.SDB.Transaction(func(tx *sqlx.Tx) error {
-		sqb := tx.Builder()
-		sqb.Insert(tt.TablePets())
-		sqb.StructNames(&pet.Pet, "id")
-		if !tx.SupportLastInsertID() {
-			sqb.Returns("id")
-		}
-		sql := sqb.SQL()
-
-		pid, err := tx.NamedCreate(sql, pet)
-		if err != nil {
-			return err
-		}
-
-		pet.ID = pid
-		if pet.File != "" {
-			fid := pet.PhotoPath()
-			sfs := tt.SFS(tx)
-			if err := sfs.DeleteFile(fid); err != nil {
-				return err
-			}
-			return sfs.MoveFile(pet.File, fid)
-		}
-		return nil
-	})
-	if err != nil {
-		c.AddError(err)
-		c.JSON(http.StatusInternalServerError, handlers.E(c))
-		return
-	}
-
-	c.JSON(http.StatusOK, xin.H{
-		"success": tbs.GetText(c.Locale, "success.created"),
-		"pet":     pet,
-	})
-}
-
-func PetUpdate(c *xin.Context) {
-	pet := petBind(c)
-	if pet.ID == 0 {
-		c.AddError(vadutil.ErrInvalidID(c))
-	}
-	if len(c.Errors) > 0 {
-		c.JSON(http.StatusBadRequest, handlers.E(c))
-		return
-	}
-
-	pet.UpdatedAt = time.Now()
-
-	tt := tenant.FromCtx(c)
-
-	var cnt int64
-	err := app.SDB.Transaction(func(tx *sqlx.Tx) error {
-		sqb := tx.Builder()
-		sqb.Update(tt.TablePets())
-		sqb.StructNames(&pet.Pet, "id", "created_at")
-		sqb.Where("id = :id")
-		sql := sqb.SQL()
-
-		r, err := tx.NamedExec(sql, pet)
-		if err != nil {
-			return err
-		}
-
-		cnt, _ = r.RowsAffected()
-
-		if pet.File != "" {
-			fid := pet.PhotoPath()
-			sfs := tt.SFS(tx)
-			if err := sfs.DeleteFile(fid); err != nil {
-				return err
-			}
-			return sfs.MoveFile(pet.File, fid)
-		}
-		return nil
-	})
-	if err != nil {
-		c.AddError(err)
-		c.JSON(http.StatusInternalServerError, handlers.E(c))
-		return
-	}
-
-	c.JSON(http.StatusOK, xin.H{
-		"success": tbs.Format(c.Locale, "pet.success.updates", cnt),
-		"pet":     pet,
-	})
-}
-
-type PetUpdatesArg struct {
-	ID     string     `json:"id,omitempty" form:"id,strip"`
-	Gender string     `json:"gender,omitempty" form:"gender,strip"`
-	BornAt *time.Time `json:"born_at,omitempty" form:"born_at"`
-	Origin string     `json:"origin,omitempty" form:"origin,strip"`
-	Temper string     `json:"temper,omitempty" form:"temper,strip"`
-	Habits *[]string  `json:"habits,omitempty" form:"habits,strip"`
-}
-
-func (pua *PetUpdatesArg) IsEmpty() bool {
-	return pua.Gender == "" && pua.BornAt == nil && pua.Origin == "" && pua.Temper == "" && pua.Habits == nil
-}
-
-func PetUpdates(c *xin.Context) {
-	pua := &PetUpdatesArg{}
-	if err := c.Bind(pua); err != nil {
-		vadutil.AddBindErrors(c, err, "pet.")
-	}
-	if pua.IsEmpty() {
-		c.AddError(tbs.Error(c.Locale, "error.request.invalid"))
-	}
-	if pua.BornAt != nil && pua.BornAt.IsZero() {
-		c.AddError(&vadutil.ParamError{
-			Param:   "born_at",
-			Message: tbs.Format(c.Locale, "error.param.required", tbs.GetText(c.Locale, "pet.born_at")),
-		})
-	}
-
-	if len(c.Errors) > 0 {
-		c.JSON(http.StatusBadRequest, handlers.E(c))
-		return
-	}
-
-	ids, ida := argutil.SplitIDs(pua.ID)
-	if len(ids) == 0 && !ida {
-		c.AddError(vadutil.ErrInvalidID(c))
-		c.JSON(http.StatusBadRequest, handlers.E(c))
-		return
-	}
-
-	tt := tenant.FromCtx(c)
-
-	sqb := app.SDB.Builder()
-	sqb.Update(tt.TablePets())
-
-	if pua.Gender != "" {
-		sqb.Setc("gender", pua.Gender)
-	}
-	if pua.BornAt != nil {
-		sqb.Setc("born_at", *pua.BornAt)
-	}
-	if pua.Origin != "" {
-		sqb.Setc("origin", pua.Origin)
-	}
-	if pua.Temper != "" {
-		sqb.Setc("temper", pua.Temper)
-	}
-	if pua.Habits != nil {
-		sqb.Setc("habits", pqx.StringArray(str.Strips(*pua.Habits)))
-	}
-	sqb.Setc("updated_at", time.Now())
-
-	if len(ids) > 0 {
-		sqb.In("id", ids)
-	}
-
-	sql, args := sqb.Build()
-
-	r, err := app.SDB.Exec(sql, args...)
-	if err != nil {
-		c.AddError(err)
-		c.JSON(http.StatusInternalServerError, handlers.E(c))
-		return
-	}
-
-	cnt, _ := r.RowsAffected()
-
-	c.JSON(http.StatusOK, xin.H{
-		"success": tbs.Format(c.Locale, "pet.success.updates", cnt),
-		"updates": pua,
-	})
-}
-
-func PetDeletes(c *xin.Context) {
-	ids, ida := argutil.SplitIDs(c.PostForm("id"))
-	if len(ids) == 0 && !ida {
-		c.AddError(vadutil.ErrInvalidID(c))
-		c.JSON(http.StatusBadRequest, handlers.E(c))
-		return
-	}
-
-	tt := tenant.FromCtx(c)
-
-	var cnt int64
-	err := app.SDB.Transaction(func(tx *sqlx.Tx) error {
-		sfs := tt.SFS(tx)
-		if len(ids) > 0 {
-			for _, id := range ids {
-				if _, err := sfs.DeletePrefix(fmt.Sprintf("/%s/%d/", models.PrefixPetFile, id)); err != nil {
-					return err
-				}
-			}
-		} else {
-			if _, err := sfs.DeletePrefix("/" + models.PrefixPetFile + "/"); err != nil {
-				return err
-			}
-		}
-
-		sqb := tx.Builder()
-		sqb.Delete(tt.TablePets())
-		if len(ids) > 0 {
-			sqb.In("id", ids)
-		}
-		sql, args := sqb.Build()
-
-		r, err := tx.Exec(sql, args...)
-		if err != nil {
-			return err
-		}
-
-		cnt, _ = r.RowsAffected()
-
-		return tt.ResetSequence(tx, "pets")
-	})
-	if err != nil {
-		c.AddError(err)
-		c.JSON(http.StatusInternalServerError, handlers.E(c))
-		return
-	}
-
-	c.JSON(http.StatusOK, xin.H{
-		"success": tbs.Format(c.Locale, "pet.success.deletes", cnt),
-	})
-}
-
-func PetDeleteBatch(c *xin.Context) {
-	pqa, err := bindPetQueryArg(c)
-	if err != nil {
-		vadutil.AddBindErrors(c, err, "pet.")
-		c.JSON(http.StatusBadRequest, handlers.E(c))
-		return
-	}
-
-	if !pqa.HasFilter() {
-		c.AddError(tbs.Error(c.Locale, "error.param.nofilter"))
-		c.JSON(http.StatusBadRequest, handlers.E(c))
-		return
-	}
-
-	tt := tenant.FromCtx(c)
-
-	var cnt int64
-	err = app.SDB.Transaction(func(tx *sqlx.Tx) (err error) {
-		sqb := tx.Builder()
-		sqb.Delete(tt.TablePets())
-		pqa.AddWhere(sqb)
-		sql, args := sqb.Build()
-
-		r, err := tx.Exec(sql, args...)
-		if err != nil {
-			return
-		}
-
-		cnt, _ = r.RowsAffected()
-
-		return tt.ResetSequence(tx, "pets")
-	})
-	if err != nil {
-		c.AddError(err)
-		c.JSON(http.StatusBadRequest, handlers.E(c))
-		return
-	}
-
-	c.JSON(http.StatusOK, xin.H{
-		"success": tbs.Format(c.Locale, "pet.success.deletes", cnt),
-	})
 }
